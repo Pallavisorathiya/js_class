dfb3ac43d09a34f754c6bba6010311477c7cbb87
Pallavisorathiya <pallavisorathiya23@gmail.com>
2025-03-25 17:56:58 +0530

    change pass

---
+++ js/chanepass.js
@@ -0,0 +1,11 @@
+// change password logic in javascript using nesteed 
+let oldpass , newpass , cpass ;
+if (oldpass == newpass){
+    alert("Passwords should not be the same");
+}
+else if (newpass != cpass){
+    alert("Passwords do not match");
+    }else{
+        alert("Password changed successfully");
+        }
+        
